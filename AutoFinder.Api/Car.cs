704332a41104b7757037f2f57a934e6989bcae4b
Paul McCallick <paul@mccallick.com>
2014-04-22 14:53:52 -0700

    renamed files and got integration testst working.

---
+++ AutoFinder.Api/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoFinder.Api
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ AutoFinder.IntegrationTests/AutoFinderIntegrationTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFinder.Api;
+using Microsoft.Owin.Hosting;
+using NUnit.Framework;
+
+namespace AutoFinder.InProcessHost
+{
+    [TestFixture]
+    public class AutoFinderIntegrationTests
+    {
+        [Test]
+        public async void GetSomeCars()
+        {
+            var baseAddress = "http://localhost:9000/";
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(baseAddress))
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client = new HttpClient();
+                var response = client.GetAsync(baseAddress + "api/car/1").Result;
+                var car = await response.Content.ReadAsAsync<Car>();
+
+                Assert.That(car.Id, Is.EqualTo(1));
+                Assert.That(car.Color, Is.EqualTo("red"));
+            }
+
+        }
+    }
+}
